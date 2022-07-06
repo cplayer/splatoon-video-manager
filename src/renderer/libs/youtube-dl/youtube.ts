9825436fcb2ebaa1b844f06e179d70e019637bf3
cplayer <zhcplayer@gmail.com>
2022-07-06 10:11:04 +0800

    修复了macos下无法正确获得youtube metadata的问题

--- src/renderer/libs/youtube-dl/youtube.ts
+++ src/renderer/libs/youtube-dl/youtube.ts
@@ -4,25 +4,35 @@ import * as fs from 'fs'
 import {spawn} from 'child_process'
 
 export function downloadYoutube (address) {
-  // 'Linux' on Linux, 'Darwin' on macOS, and 'Windows_NT' on Windows
+  // 'Darwin' on macOS, and 'Windows_NT' on Windows
   var filePath = ''
   if (os.type() === 'Windows_NT') {
     filePath = path.join(__static, '/yt-dlp.exe')
   } else if (os.type() === 'Darwin') {
     filePath = path.join(__static, '/yt-dlp_macos')
   }
-  let child = spawn(filePath + ' --skip-download --dump-json "' + address + '"', {
+  console.log('filePath', filePath)
+  let child = spawn(filePath, [
+    '--skip-download',
+    '--dump-json',
+    address
+  ], {
     shell: process.platform === 'win32'
   })
+  var randomId = 'abcdefg'
+  var outputFile = path.join(__static, '/output_' + randomId + '.info.json')
+  if (fs.existsSync(outputFile)) {
+    fs.unlinkSync(outputFile)
+  }
   child.stdout.on('data', (data) => {
     console.log('开始写入文件...')
-    fs.writeFile(path.join(__static, '/output.info.json'), data, {
+    fs.appendFile(outputFile, data, {
       encoding: 'utf8'
     }, (err) => {
       if (err) {
-        process.stdout.write('文件写入失败！', err.message)
+        console.log('写入文件失败！', err.message)
       } else {
-        process.stdout.write('文件写入成功！')
+        console.log('写入文件成功！')
       }
     })
   })
